3cba2b02b7881c7d01f14e8833ca71d7a5b886af
mobinazarif <76546618+mobinazarif@users.noreply.github.com>
2021-12-12 14:43:11 +0330

    Create dodoee.cpp

---
+++ dodoee.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int l, int r, int x)
+{
+	if (r >= l) {
+		int mid = l + (r - l) / 2;
+		if (arr[mid] == x)
+			return mid;
+		if (arr[mid] > x)
+			return binarySearch(arr, l, mid - 1, x);
+		return binarySearch(arr, mid + 1, r, x);
+	}
+	return -1;
+}
+
+int main(void)
+{
+	int arr[] = { 3, 26, 12, 67, 413 };
+	int x = 67;
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int result = binarySearch(arr, 0, n - 1, x);
+	(result == -1)
+		? cout << "item is not found"
+		: cout << "Element  index is = " << result<<endl;
+	system("pause");
+}
